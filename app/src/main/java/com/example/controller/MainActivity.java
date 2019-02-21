f4b939718b9c7b376298403044e73f98b72ab278
raffschlesser <raffschlesser1@gmail.com>
2019-02-21 17:30:33 +0100

    tcp connection added

--- app/src/main/java/com/example/controller/MainActivity.java
+++ app/src/main/java/com/example/controller/MainActivity.java
@@ -2,21 +2,50 @@ package com.example.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import io.github.controlwear.virtual.joystick.android.JoystickView;
 
 public class MainActivity extends AppCompatActivity {
+    TCPConnection connection = new TCPConnection("192.168.0.1", 156);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         joystickListener();
+        buttonListener();
     }
 
+    public void buttonListener(){
+        //connection button
+        final Button connectButton = findViewById(R.id.connect_button);
+        connectButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View v){
+                if (!connection.isConnected()) {
+                    String resp = connection.startConnection();
+                    if ("connection succes" == resp) {
+                        connectButton.setText("disconnect");
+                    }
+                    Toast.makeText(getApplicationContext(), resp, Toast.LENGTH_SHORT).show();
+                } else {
+                    String resp = connection.stopConnection();
+                    if ("disconnect succes" == resp) {
+                        connectButton.setText("connect");
+                    }
+                    Toast.makeText(getApplicationContext(), resp, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
     public void joystickListener(){
-        JoystickView joystick1 = (JoystickView) findViewById(R.id.joystick1);
+        JoystickView joystick1 = findViewById(R.id.joystick1);
         joystick1.setOnMoveListener(new JoystickView.OnMoveListener() {
             @Override
             public void onMove(int angle, int strength) {
@@ -24,10 +53,12 @@ public class MainActivity extends AppCompatActivity {
                 int yStrength = (int)  Math.round(strength * Math.cos(Math.toRadians(angle)));
                 int xStrength = (int) Math.round(strength * Math.sin(Math.toRadians(angle)));
                 joystickText.setText("x"+ xStrength + " y" + yStrength);
+                String resp = connection.sendMessage("x"+ xStrength + " y" + yStrength);
+              //  if(resp!=null) Toast.makeText(getApplicationContext(), resp, Toast.LENGTH_SHORT).show();
             }
         });
 
-        JoystickView joystick2 = (JoystickView) findViewById(R.id.joystick2);
+        JoystickView joystick2 = findViewById(R.id.joystick2);
         joystick2.setOnMoveListener(new JoystickView.OnMoveListener() {
             @Override
             public void onMove(int angle, int strength) {
@@ -35,6 +66,8 @@ public class MainActivity extends AppCompatActivity {
                 int yStrength = (int)  Math.round(strength * Math.cos(Math.toRadians(angle)));
                 int xStrength = (int) Math.round(strength * Math.sin(Math.toRadians(angle)));
                 joystickText.setText("x"+ xStrength + " y" + yStrength);
+                String resp = connection.sendMessage("x"+ xStrength + " y" + yStrength);
+              //  if(resp!=null) Toast.makeText(getApplicationContext(), resp, Toast.LENGTH_SHORT).show();
             }
         });
     }
---
+++ app/src/main/java/com/example/controller/TCPConnection.java
@@ -0,0 +1,63 @@
+package com.example.controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class TCPConnection {
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+    private String ip;
+    private int port;
+
+
+    public TCPConnection(String ip, int port) {
+        this.ip = ip;
+        this.port = port;
+    }
+
+    public String sendMessage(String msg) {
+        String resp;
+        try {
+        out.println(msg);
+            resp = in.readLine();
+        }catch (Exception e){
+            resp="failed, no connection";
+        }
+        return resp;
+    }
+
+    public String startConnection() {
+        String resp ="succes";
+        try{
+            clientSocket = new Socket(ip, port);
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        }catch (Exception e){
+            resp="connecting failed";
+        }
+        return resp;
+    }
+
+    public String stopConnection() {
+        String resp = "connection succes";
+        try{
+            in.close();
+            out.close();
+            clientSocket.close();
+            clientSocket=null;
+        }catch (Exception e){
+            resp="disconnecting failed";
+        }
+        return resp;
+    }
+    public boolean isConnected(){
+        boolean resp = true;
+        if(clientSocket==null)resp=false;
+        return resp;
+    }
+
+}
